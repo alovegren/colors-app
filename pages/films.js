d9702835a6b2de504a3f658315af1c259839e9f5
Missy Lovegren <audrey.lovegren@gmail.com>
2022-07-05 17:07:58 -0500

    Remove films route (ssr)

--- pages/films.js
+++ pages/films.js
@@ -1,23 +1,23 @@
-import React from 'react'
+// import React from 'react'
 
-const Film = ({ data }) => {
-  return (
-    <main>
-      <h1>The Starwars films... happy!</h1>
-      <ul>
-        {data.map(item => (
-          <li key={item.title}>{item.title}</li>
-        ))}
-      </ul>
-    </main>
-  )
-}
+// const Film = ({ data }) => {
+//   return (
+//     <main>
+//       <h1>The Starwars films... happy!</h1>
+//       <ul>
+//         {data.map(item => (
+//           <li key={item.title}>{item.title}</li>
+//         ))}
+//       </ul>
+//     </main>
+//   )
+// }
 
-export async function getServerSideProps () {
-  const response = await fetch('https://swapi.dev/api/films/')
-  const data = await response.json()
+// export async function getServerSideProps () {
+//   const response = await fetch('https://swapi.dev/api/films/')
+//   const data = await response.json()
 
-  return { props: { data: data.results } }
-}
+//   return { props: { data: data.results } }
+// }
 
-export default Film
+// export default Film
