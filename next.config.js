811bdb1dbea35dd98f0d4b0c6a332e7f465362f2
Missy Lovegren <audrey.lovegren@gmail.com>
2022-07-08 11:41:53 -0500

    add kitten secret

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    FAVORITE_MOVIE: process.env.FAVORITE_MOVIE,
+  }
 }
 
 module.exports = nextConfig
